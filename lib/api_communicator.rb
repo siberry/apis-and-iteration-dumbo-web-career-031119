4aa302c69985565d642adb4c2ae46ff5488a5f45
siberry <siberry226@gmail.com>
2019-03-12 16:03:26 -0400

    Update CLI to account for pages of API

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,36 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def response_hash(page_number)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get("https://www.swapi.co/api/people/?page=#{page_number}")
   response_hash = JSON.parse(response_string)
+end
+
+def characters_on_page(page_number)
+  characters = []
+  response_hash(page_number)["results"].each do |character_hash|
+    characters << character_hash["name"]
+  end
+  characters
+end
+
+#characters_on_page(2)
+
+def get_page_number(character_name) #return the page number
+  page_number = 1
+  until characters_on_page(page_number).include?(character_name)
+    page_number += 1
+  end
+  page_number
+end
+
+#get_page_number("Wilhuff Tarkin")
+
+
+def get_character_movies_from_api(character_name)
+  page_number = get_page_number(character_name)
+  response_hash = response_hash(page_number)
   #return the character hash from API
   character_hash = response_hash["results"].find do |character_hash|
     character_hash["name"] == character_name
@@ -15,8 +41,8 @@ def get_character_movies_from_api(character_name)
     url_response_string = RestClient.get(url)
     JSON.parse(url_response_string)
   end
+  character_movies_hash
 end
-
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
